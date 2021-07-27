c1e6972092725eef37d5583be2c02abdf61a3e35
ZeroVenom776 <cmastered44@gmail.com>
2021-07-27 18:45:06 +0300

    Added ironman file.

---
+++ src/main/java/baritone/utils/Ironman.java
@@ -0,0 +1,53 @@
+/*
+ * This file is part of Bettertone.
+ *
+ * Bettertone is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Bettertone is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Bettertone.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package baritone.utils;
+
+import baritone.api.BaritoneAPI;
+import baritone.api.event.events.RenderEvent;
+import baritone.api.pathing.calc.IPath;
+import baritone.api.pathing.goals.*;
+import baritone.api.utils.BetterBlockPos;
+import baritone.api.utils.Helper;
+import baritone.api.utils.interfaces.IGoalRenderPos;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.tileentity.TileEntityBeaconRenderer;
+import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
+import net.minecraft.entity.Entity;
+import net.minecraft.init.Blocks;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
+
+import java.awt.*;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+import static org.lwjgl.opengl.GL11.*;
+
+/**
+ * @author Cae
+ */
+public final class PathRenderer implements IRenderer {
+
+    private PathRenderer() {}
+
+    public static void render(RenderEvent event) {
+
+    }
